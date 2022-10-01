cad8ddec10b15b28b5b29a2f78b6750fb12b000d
Gaurav Agarwal <algogrit@gmail.com>
2022-10-01 18:41:00 +0530

    Updating summer example.

--- examples/cpp/lambdas/summer.cc
+++ examples/cpp/lambdas/summer.cc
@@ -1,29 +1,27 @@
 #include <functional>
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 const function<int(int)> summer() {
   int sum = 0; // When does this get deallocated?
 
-  return [sum](int x) mutable -> int { // x = 20 | sum = 11
-    sum = sum + x;
+  // printf("%p\n", &sum);
+
+  return [sum](int x) mutable -> int {
+    // printf("%p\n", &sum);
+
+    sum += x;
     return sum;
   };
 }
 
 int main() {
   auto sum1 = summer(); // Type: ?
-  auto sum2 = summer(); // Type: ?
 
-  cout << sum1(11) << "\n"; // Value: ? | Line: ?
-  cout << sum1(20) << "\n"; // Value: ? | Line: ?
-
-  // cout << sum1(30) << "\n"; // Value: ?
-
-  // cout << sum2(12) << "\n"; // Value: ?
-  // cout << sum2(20) << "\n"; // Value: ?
-  // cout << sum2(30) << "\n"; // Value: ?
+  cout << sum1(11) << "\n"; // Value: ?
+  cout << sum1(10) << "\n"; // Value: ?
 
   return 0;
 }
